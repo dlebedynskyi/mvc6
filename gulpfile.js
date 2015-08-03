0eb9adae4554f724f88b2a39e79031c9ede61334
Dmytro Lebedynskyi <dima.lebedinsky@gmail.com>
2015-08-03 14:15:33 -0400

    publish scripts
    added main bower files publish with gulp
    added  project.json publish settings
    gulp task build:lib and prod:lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /// <binding Clean='clean' />
 'use strict';
 var gulp = require("gulp"),
+  gutil = require("gulp-util"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
@@ -9,12 +10,16 @@ var gulp = require("gulp"),
   project = require("./project.json"),
   changed = require('gulp-changed'),
   notify = require('gulp-notify'),
-  plumber = require('gulp-plumber');
+  plumber = require('gulp-plumber'),
+  mainBowerFiles = require('main-bower-files');
 
 var paths = {
-  webroot: "./" + project.webroot + "/"
+    webroot: "./" + project.webroot + "/",
+    bowerfile: "./bower.json",
+    
 };
 
+paths.webrootLib = paths.webroot + 'lib/';
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.less = paths.webroot + "less/**/*.less";
@@ -32,7 +37,11 @@ gulp.task("clean:css", function(cb) {
   rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean:lib", function (cb) {
+    rimraf(paths.webrootLib, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:lib"]);
 
 
 gulp.task('build:less', function () {
@@ -41,11 +50,27 @@ gulp.task('build:less', function () {
         .pipe(changed(paths.lessDest, { extension: '.css' }))
         .pipe(less({
             sourceMap: {
-                sourceMapRootpath: '../less' // Optional absolute or relative path to your LESS files 
+                sourceMapRootpath: '../less' 
             }
         }))
         .pipe(gulp.dest(paths.lessDest));
 });
+
+gulp.task('build:lib',["clean:lib"], function () {
+    var env = gutil.env.env || "development";
+    log('build for ' + env);
+    return gulp.src(mainBowerFiles({ env: env}), { base: './bower_components' })
+               .pipe(gulp.dest(paths.webrootLib));
+});
+
+gulp.task('build', ["build:less", "build:lib"]);
+
+
+gulp.task("prod:lib", ["clean:lib"], function () {
+    return gulp.src(mainBowerFiles({ env: "production"}), { base: './bower_components' })
+             .pipe(gulp.dest(paths.webrootLib));
+});
+
 
 gulp.task("min:js", function() {
   gulp.src([paths.js, "!" + paths.minJs], {
@@ -68,3 +93,16 @@ gulp.task('watch:less', function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+
+function log(msg) {
+    if (typeof (msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                gutil.log(gutil.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        gutil.log(gutil.colors.blue(msg));
+    }
+}
